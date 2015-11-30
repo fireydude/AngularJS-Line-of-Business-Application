bb9b74c9fa5bd6de985d2f5f5eb12b71abd59309
Brian Herbert <Brian.Herbert@webuyanycar.com>
2015-11-30 14:33:58 +0000

    Validating Forms

--- app/products/ProductEditCtrl.js
+++ app/products/ProductEditCtrl.js
@@ -21,7 +21,7 @@
             vm.title = "Edit: " + vm.product.productName;
         }
         else {
-            vm.title = "New Product"
+            vm.title = "New Product";
         }
 
         vm.open = function ($event) {
@@ -31,30 +31,32 @@
             vm.opened = !vm.opened;
         };
 
-        vm.submit = function () {
-            vm.product.$save(function (data) {
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.product.$save(function (data) {
                     toastr.success("Save Successful");
-                }
-            );
-        }
+                })
+            } else {
+                alert("Please correct the validation errors first.");
+            }
+        };
 
         vm.cancel = function () {
             $state.go('productList');
-        }
+        };
 
         vm.addTags = function (tags) {
             if (tags) {
                 var array = tags.split(',');
                 vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
                 vm.newTags = "";
-            } else{
+            } else {
                 alert("Please enter one or more tags separated by commas");
             }
-        }
+        };
 
         vm.removeTag = function (idx) {
             vm.product.tags.splice(idx, 1);
-        }
-
+        };
     }
 }());
